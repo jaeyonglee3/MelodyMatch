6a029a5ad0e57dfaaf10fc938c5462cbc2c794a0
kev0921 <kevin.hu0921@gmail.com>
2023-03-27 16:38:32 -0400

    Added print statement

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,15 @@
 # Decision Tree
 
-import csv
+import csv, random
+from typing import Optional
+
+
+
+
+
+
+
+
+
 def read_and_write_csv(csv: str) -> None:
     """Reads data from our songs dataset"""
