acecb0816b07d04adc83cc203b6d81d101b713f9
kev0921 <kevin.hu0921@gmail.com>
2023-03-27 15:51:25 -0400

    Added print statement

--- main.py
+++ main.py
@@ -15,5 +15,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('Hell000o0')
+    print('Hell000o0')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
